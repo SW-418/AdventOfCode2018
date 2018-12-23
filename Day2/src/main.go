52e04f508c28474729047e71587ae5c959c82497
Sam Wells <samwells95@gmail.com>
2018-12-23 01:48:09 +0000

    Add solution for day2

---
+++ Day2/src/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fileName := "frequency.txt"
+	file, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		unsplitString := string(file)
+		splitString := strings.Split(unsplitString, "\n")
+		resultingFrequency := 0
+		frequencyMap := make(map[int]int)
+		duplicateFrequencyFound := false
+		for index := 0; !duplicateFrequencyFound; index++ {
+			stringConversion, err := strconv.Atoi(splitString[index])
+			if err != nil {
+				fmt.Println(err)
+			}
+			resultingFrequency += stringConversion
+			if frequencyMap[stringConversion] != 0 {
+				duplicateFrequencyFound = true
+			} else {
+				frequencyMap[stringConversion] = 1
+			}
+			if index == len(splitString)-1 {
+				index = 0
+			}
+		}
+		fmt.Println(resultingFrequency)
+	}
+}
